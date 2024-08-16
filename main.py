255f9a338f2dcda291296acc61d4938d5bf42d5f
omeryenipazar <yenipazaromer@gmail.com>
2024-08-16 16:13:36 +0300

    Add unhealthy application for check liveness and readiness probe

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Response, status
 from typing import Dict, Any
 
 app = FastAPI()
@@ -8,8 +8,9 @@ def read_root():
     return {"msg": "BC4M"}
 
 @app.get("/health")
-def read_health():
-    return {"status": "healthy"}
+def read_health(response: Response):
+    response.status_code = status.HTTP_503_SERVICE_UNAVAILABLE
+    return {"status": "unhealthy"}
 
 @app.post("/data")
 def receive_data(data: Dict[str, Any]):
